3dcdcb3edcff363f2553142088244092431b3f1e
Menservant <47711994+Menservant@users.noreply.github.com>
2021-08-23 20:13:39 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,11 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
+import math
+
+
+def pagination(num_items, items_on_page):
+    last_page = num_items % items_on_page
+    return last_page
+
+
 
