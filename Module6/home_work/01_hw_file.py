2772d91e141eeb90f8b3d0b204984af12aa66e4c
Menservant <47711994+Menservant@users.noreply.github.com>
2021-08-25 15:16:14 +0300

    Update 01_hw_file.py

--- Module6/home_work/01_hw_file.py
+++ Module6/home_work/01_hw_file.py
@@ -1,8 +1,10 @@
 # Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
 
+
 def log(text, file="log.txt"):
-    pass
+    with open(f"{file}", "a", encoding="utf-8") as f:
+            f.write(f"{text} \n")
 
 
 log("hello world")  # дописывает "hello world" в конец файла log.txt
-log("message", "log01.txt")  # дописывает "message" в конец файла log01.txt
+log("message", "log011.txt")  # дописывает "message" в конец файла log01.txt
