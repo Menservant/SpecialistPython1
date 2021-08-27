3daefb057fdf823ca3f0ae9c29c1e6130ec5ed0b
Menservant <47711994+Menservant@users.noreply.github.com>
2021-08-27 21:00:31 +0300

    Update 01_task_result.py

--- Module10/practice/result/01_task_result.py
+++ Module10/practice/result/01_task_result.py
@@ -2,8 +2,8 @@
 # Формат входных данных: даны три целые числа - ширина, высота и длина параллелепипеда.
 # Формат выходных данных: вывести площадь всех граней и объем фигуры
 
-# Важно!
-# Оформите решение так, что работа с вашей программой была удобна пользователю.
-# Пользователь должен понимать, что его просят ввести и что именно делает программа.
-
-
+width = int(input("Enter width of figure"))
+high = int(input("Enter high of figure"))
+length = int(input("Enter length of figure"))
+print(f"Volume of figure = {width * high * length}")
+print(f"Square of figure = {2 * ((width * length)+(length * high)+(width*high))}")
